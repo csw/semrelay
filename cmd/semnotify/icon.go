0b10ed046fc6be923332e29333d4a06e6efba1da
Clayton Wheeler <cswheeler@gmail.com>
2021-11-22 19:24:07 -0600

    Include icon alpha channel

--- cmd/semnotify/icon.go
+++ cmd/semnotify/icon.go
@@ -12,21 +12,14 @@ type DBusIcon struct {
 	Data          []uint8
 }
 
-// buildIcon builds a DBus icon from a Go RGBA image, discarding the alpha
-// channel. DBus notifications supposedly support alpha channels, but at least
-// with Mako I haven't been able to get them to display correctly.
 func buildIcon(img *image.NRGBA) *DBusIcon {
-	raw := []uint8{}
-	for i := 0; i < img.Rect.Dx()*img.Rect.Dy(); i++ {
-		raw = append(raw, img.Pix[i*4:i*4+3]...)
-	}
 	return &DBusIcon{
 		Width:         img.Rect.Dx(),
 		Height:        img.Rect.Dy(),
-		Rowstride:     3 * img.Rect.Dx(),
-		HasAlpha:      false,
+		Rowstride:     4 * img.Rect.Dx(),
+		HasAlpha:      true,
 		BitsPerSample: 8,
-		Channels:      3,
-		Data:          raw,
+		Channels:      4,
+		Data:          img.Pix,
 	}
 }
